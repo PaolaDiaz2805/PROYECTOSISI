b0e022f7a2c21608f43b8130f3bb7a15fb969174
valee33 <95valeria78@gmail.com>
2025-07-31 15:42:34 -0400

    cambio_dos

--- FormSession.php
+++ FormSession.php
@@ -44,50 +44,8 @@
 </script>
   </header>
   <div class="cuerpo">
-  <section class="b_izquierda">
-  <nav class="barra_izq">
-        <img src="FOTOS/logo_casa.png" class="casa">
-        <h2 class="nom">Menú</h2>
-        <div class="men" >
-            <div class="sub">
-                <div class="no"><h3 class="di">Horario de Clases</h3> </div>
-            <div class="no"><h3 class="di">Calendario</h3></div>  
-            <div class="no"><h3 class="di">Profesores</h3> </div>
-            <div class="no"><h3 class="di">Guias de Curso</h3> </div>
-            <div class="no"><h3 class="di">Himno al Colegio</h3></div>
-            </div>
-            
-        </div>
-        <h2 class="nam">Noticias</h2>
-        <aside class="noti">
-            <h3 class="di">Inicio de año Escolar</h3>
-            <h3 class="di">Requisitos para <br> Matricularse</h3>
-            <h3 class="di">Matriculas</h3>
-            <h3 class="di">Inscripciones</h3>
-        </aside>
-        <h2>Visitas</h2>
-        <aside class="tabla">
-            <div class="visi">
-                <table>
-  <thead>
-    <tr>
-      <th>HOY</th>
-      <th>ESTE MES</th>
-      <th>MES PASADO</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>4</td>
-      <td>55</td>
-      <td>230</td>
-    </tr>
-  </tbody>
-</table>
-            </div>
-        </aside>
-    </nav>
-  </section>
+  
+  <?php include("barra_iz.php"); ?>
   <section class="centro">
     
   <div class="espacios">
--- form_rgis copy.php
+++ form_rgis copy.php
@@ -1,8 +1,7 @@
-
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
   <title>Alumno</title>
@@ -14,15 +13,12 @@
  
 </head>
 
-<body>
-
-    
-
+<body class="bo">
  
-  <header>
+  <header class="hea">
         <div class="encabezado"> 
                     <img src="FOTOS/logo.jpeg" class ="logo_cole"/>
-                    <a href="test.html" class="titulo">U.E. RENÉ BARRIENTOS</a>
+                    <a href="" class="titulo">U.E. RENÉ BARRIENTOS</a>
                 </div>
             <div class="menu">
                     <button onclick="toggleMenu()" class="menu-boton"><img src="FOTOS/barras.png" style=" width: 50px "></button>
@@ -49,53 +45,11 @@
 </script>
   </header>
   <div class="cuerpo">
-  <section class="b_izquierda">
-  <nav class="barra_izq">
-        <img src="FOTOS/logo_casa.png" class="casa">
-        <h2 class="nom">Menú</h2>
-        <div class="men" >
-            <div class="sub">
-                <div class="no"><h3 class="di">Horario de Clases</h3> </div>
-            <div class="no"><h3 class="di">Calendario</h3></div>  
-            <div class="no"><h3 class="di">Profesores</h3> </div>
-            <div class="no"><h3 class="di">Guias de Curso</h3> </div>
-            <div class="no"><h3 class="di">Himno al Colegio</h3></div>
-            </div>
-            
-        </div>
-        <h2 class="nam">Noticias</h2>
-        <aside class="noti">
-            <h3 class="di">Inicio de año Escolar</h3>
-            <h3 class="di">Requisitos para <br> Matricularse</h3>
-            <h3 class="di">Matriculas</h3>
-            <h3 class="di">Inscripciones</h3>
-        </aside>
-        <h2>Visitas</h2>
-        <aside class="tabla">
-            <div class="visi">
-                <table>
-  <thead>
-    <tr>
-      <th>HOY</th>
-      <th>ESTE MES</th>
-      <th>MES PASADO</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>4</td>
-      <td>55</td>
-      <td>230</td>
-    </tr>
-  </tbody>
-</table>
-            </div>
-        </aside>
-    </nav>
-  </section>
+
+  
+
   <section class="centro">
-    
-            
+
 
         <div class="espacios">
     
